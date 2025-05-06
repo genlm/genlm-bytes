07e86f59f24468b1c0fa75d730bfc5c2d76227b7
benlebrun <benlebrun1@gmail.com>
2025-05-06 10:27:26 -0400

    more tests

--- tests/test_beam.py
+++ tests/test_beam.py
@@ -1,4 +1,7 @@
 import pytest
+import time
+import numpy as np
+import asyncio
 from genlm.backend import load_model_by_name
 from genlm.tokenization.byte_lm import ByteBeamState
 
@@ -29,5 +32,57 @@ async def test_generate(llm):
         )
         print(repr(output))
         assert output == b"An apple a day keeps the doctor away."
+    finally:
+        await state.cleanup()
+
+
+@pytest.mark.asyncio
+async def test_async_batching(llm):
+    state = await ByteBeamState.initial(llm, K=5)
+
+    try:
+        # warm up
+        await state.greedy(b"I", steps=5)
+        await state.greedy(b"Y", steps=5)
+
+        start = time.time()
+        concurrent_output = await asyncio.gather(
+            state.greedy(b"I", steps=5),
+            state.greedy(b"Y", steps=5),
+        )
+        concurrent_time = time.time() - start
+
+        start = time.time()
+        sequential_output_I = await state.greedy(b"I", steps=5)
+        sequential_output_Y = await state.greedy(b"Y", steps=5)
+        sequential_time = time.time() - start
+
+        print(f"Concurrent requests time: {concurrent_time:.2f} seconds")
+        print(f"Sequential requests time: {sequential_time:.2f} seconds")
+
+        assert concurrent_output == [sequential_output_I, sequential_output_Y]
+        assert concurrent_time < sequential_time
+    finally:
+        await state.cleanup()
+
+
+@pytest.mark.asyncio
+async def test_weights(llm):
+    state = await ByteBeamState.initial(llm, K=5)
+
+    try:
+        qs = b"An apple a day keeps the"
+        for q in qs:
+            state = await state.step(q)
+            for candidate in state.states:
+                context = candidate.lm_state.context
+                llm = candidate.lm_state.model
+                want = 0
+                for i in range(1, len(context)):
+                    logps = await llm.next_token_logprobs(context[:i])
+                    want += logps[context[i]]
+                want += candidate.mass[candidate.node]
+                assert np.isclose(want, candidate.weight, rtol=0.01)
+            state = state.prune()
     finally:
         await state.cleanup()
